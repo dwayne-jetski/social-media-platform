617b89dbd01bb481e30b9cd35a28946f31ba842a
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2021-01-13 20:51:08 -0500

    new models page userinfo and added schemas

--- social-media-backend/index.js
+++ social-media-backend/index.js
@@ -1,3 +1,14 @@
 const connectDB = require('./startup/db');
+const express = require('express');
+const app = express();
+
+app.listen(3000, function () {
+    console.log("Server started. Listening on port 3000.");
+})
+
+app.get("/api/products", (req, res) => {
+    let products = repoContext.products.findAllProducts();
+    res.send(products);
+})
 
 connectDB();
---
+++ social-media-backend/models/userInfo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userInfoSchema = new mongoose.Schema({
+    firstName: { type: String, required: true, minlength: 1, maxlength: 15 },
+    lastName: { type: String, required: true, minlength: 1, maxlength: 15 },
+    eMail: { type: String, required: true, minlength: 1, maxlength: 25, unique: true },
+    userName: { type: String, required: true, minlength: 8, maxlength: 15, unique: true },
+    password: { type: String, required: true, minlength: 8, maxlength: 10 },
+    profileInfo: { profileDate }
+
+
+})
+
+const profileDate = new mongoose.Schema({
+    posts: { default: [], type: String, minlength: 1, maxlength: 240 },
+    aboutMe: { default: "", type: String, minlength: 1, maxlength: 800 },
+    likedMusic: { default: [], type: String, minlength: 1, maxlength: 15 },
+    dislikedMusic: { default: [], type: String, minlength: 1, maxlength: 15 },
+    profilePic: { default: null}
+})
