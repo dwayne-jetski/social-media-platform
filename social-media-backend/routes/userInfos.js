a6b2730bd0cfbe487de533eebf76bffb3cb45324
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2021-01-18 19:26:57 -0500

    added get requests

--- social-media-backend/routes/userInfos.js
+++ social-media-backend/routes/userInfos.js
@@ -2,6 +2,29 @@ const { UserInfo, validate } = require('../models/userinfo');
 const express = require('express');
 const router = express.Router();
 
+router.get('/', async (req, res) =>{
+    try {
+        const userInfos = await UserInfo.find();
+        return res.send(userInfos);
+    } catch (ex) {
+        return res.status(500).send('Internal Server Error: ${ex}');
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+
+        const product = await product.findByID(req.params.id);
+
+        if (!userInfo)
+        return res.status(400).send('The userInfo with id "${req.params.id}" does not exist.');
+
+        return res.send(userInfo);
+    } catch (ex) {
+        return res.status(500).send('Internal Server Error: ${ex}');
+    }
+});
+
 router.post('/', async (req, res) => {
     try {
         const { error } = validate(req.body);
@@ -54,3 +77,48 @@ router.post('/', async (req, res) => {
 });
 
 module.exports=router;
+
+router.put('/:id', async (req,res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error);
+
+        const userInfo = await UserInfo.findByIdAndUpdate(
+            req.params.id,
+            {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                userName: req.body.userName,
+                password: req.body.password,
+                profileInfo: req.body.profileInfo,
+            },
+            { new: true }
+        );
+
+        if (!userInfo)
+        return res.status(400).send('The userInfo with id "${req.params.id}" does not exist.');
+
+        await userInfo.save();
+
+        return res.send(userInfo);
+    } catch (ex) {
+        return res.status(500).send('Internal Server Error: ${ex}');
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+
+        const userInfo = await UserInfo.findByIdAndRemove(req.params.id);
+
+        if (!userInfo)
+        return res.status(400).send('The userInfo with id "${req.params.id}" does not exist.');
+
+        return res.send(userInfo);
+
+    } catch (ex) {
+        return res.status(500).send('Internal Server Error: ${ex}');
+
+    }
+});
