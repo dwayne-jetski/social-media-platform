77b4212f22424c0d67c3ec663e2fcc79cc82da3a
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2021-01-19 21:46:01 -0600

    added axios post to front end for creating profile, but it's not quite working yet.

--- social-media-frontend/src/components/CreateAccountPage/createAccount.jsx
+++ social-media-frontend/src/components/CreateAccountPage/createAccount.jsx
@@ -4,15 +4,34 @@ import FormControl from 'react-bootstrap/FormControl'
 import Form from 'react-bootstrap/Form';
 import {Col, Row} from 'react-bootstrap';
 import useForm from '../useForm/useForm';
-import './createAccount.css'
+import axios from 'axios';
 
 
 function CreateAccount(props){    
 
     const register = () =>{
+        console.log("Email: ", values.email);
+        console.log("User Name: ", values.userName);
+        console.log('First Name: ', values.firstName);
+        console.log('Last Name: ', values.lastName);
+        console.log("Password: ", values.password);
+        console.log("Password Confirmation: ", values.confirmPassword);
 
         if(values.password === values.confirmPassword){
-            alert(`Thanks for registering! Check your email ${values.email} for confirmation`);            
+            alert(`Thanks for registering! Check your email ${values.email} for confirmation`);      
+            
+            const user = {
+                firstName: values.firstName,
+                lastName: values.lastName,
+                email: values.email,
+                userName: values.userName,
+                password: values.password
+            }
+
+            axios.post('http://localhost:5000/api/userInfo/', user)
+            .then(res => {
+                console.log(res);
+            });
         
             props.useRenderedPage('landingPage');
         } else if(values.pasword !== values.confirmPassword){
@@ -29,24 +48,25 @@ function CreateAccount(props){
             <Row className="jumbotron" >
                 <Col xs={3} />
                 <Col xs={6} className="create_account_form"> 
+                    
                     <Col >               
                         <Form onSubmit={handleSubmit}>
                             <h1>Register Now</h1>
                             <br/>
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" name="email" onChange={handleChange} value={values.email} required={true}/>
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="username" placeholder="Enter username" name="userName" onChange={handleChange} value={values.userName} required={true}/>
                             <Form.Label>First Name</Form.Label>
                             <Form.Control type="firstName" placeholder="Enter first name" name="firstName" onChange={handleChange} value={values.firstName} required={true}/>
                             <Form.Label>Last Name</Form.Label>
                             <Form.Control type="lastName" placeholder="Enter last name" name="lastName" onChange={handleChange} value={values.lastName} required={true}/>
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" name="email" onChange={handleChange} value={values.email} required={true}/>
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="username" placeholder="Enter username" name="userName" onChange={handleChange} value={values.userName} required={true}/>
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Enter password 8-32 characters long" name="password" onChange={handleChange} value={values.password} required={true}/>
                             <Form.Label className={values.textColor}>Confirm Password</Form.Label>
                             <Form.Control type="password" placeholder="Confirm your password" name="confirmPassword" onChange={handleChange} value={values.confirmPassword} required={true}/>
                             <br/>
-                            <Button variant="outline-primary" type="submit">Register</Button>
+                            <Button type="submit">Register</Button>
                         </Form>
                     </Col>
                 </Col>
